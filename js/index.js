cc033ecc7757f3e3b2351985d02a81c89ca2d524
lmaccaro-maker <lauramaccarone91@gmail.com>
2025-04-10 20:49:34 +0200

    loading

--- js/index.js
+++ js/index.js
@@ -3,13 +3,39 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+// Obtener el precio del producto
+const price = parseFloat(product.querySelector('.price span').innerText);
+
+// Obtener el valor de cantidad
+const quantity = product.querySelector('.quantity input').valueAsNumber;
+
+// Calcular subtotal
+const subtotal = price * quantity;
+
+// Establecer el subtotal resultante en el DOM
+product.querySelector('.subtotal span').innerText = subtotal.toFixed(2);
+
 }
+
+// Selecciona el elemento del producto fuera de la función
+const productElement = document.querySelector('.product');
+
+// Llama a la función una vez para probarlo
+updateSubtotal(productElement);
+
+// Añadir un event listener para el input de cantidad
+const quantityInput = productElement.querySelector('.quantity input');
+quantityInput.addEventListener('change', () => updateSubtotal(productElement));
+
+
+
+//
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
+  
   updateSubtotal(singleProduct);
   // end of test
 
