74a13a3a428e1b27a93d29577c5f4304ae401ac1
lmaccaro-maker <lauramaccarone91@gmail.com>
2025-04-12 12:24:05 +0200

    updated

--- js/index.js
+++ js/index.js
@@ -3,40 +3,27 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-// Obtener el precio del producto
-const price = parseFloat(product.querySelector('.price span').innerText);
+  // Obtener el precio del producto
+  const price = parseFloat(product.querySelector('.price span').innerText);
 
-// Obtener el valor de cantidad
-const quantity = product.querySelector('.quantity input').valueAsNumber;
+  // Obtener el valor de cantidad
+  const quantity = product.querySelector('.quantity input').valueAsNumber;
 
-// Calcular subtotal
-const subtotal = price * quantity;
+  // Calcular subtotal
+  const subtotal = price * quantity;
 
-// Establecer el subtotal resultante en el DOM
-product.querySelector('.subtotal span').innerText = subtotal.toFixed(2);
-
+  // Establecer el subtotal resultante en el DOM
+  product.querySelector('.subtotal span').innerText = subtotal.toFixed(2);
 }
-
-// Selecciona el elemento del producto fuera de la función
-const productElement = document.querySelector('.product');
-
-// Llama a la función una vez para probarlo
-updateSubtotal(productElement);
-
-// Añadir un event listener para el input de cantidad
-const quantityInput = productElement.querySelector('.quantity input');
-quantityInput.addEventListener('change', () => updateSubtotal(productElement));
-
-
 
 //
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  
-  updateSubtotal(singleProduct);
+  const singleProduct = document.querySelectorAll('.product');
+
+  for(let i=0; i<singleProduct.length; i++) updateSubtotal(singleProduct[i]);
   // end of test
 
   // ITERATION 2
@@ -52,12 +39,27 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  // closest();
+  // remove();
+
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
+  /*
+  if(!name || isNaN(price)) {
+    alert("Algo ha ido mal")
+    return;
+  }
+  */
   //... your code goes here
+  // 1º Crear el form del HTML
+  // 2º Acceder a los valores del form
+  // 3º creas el HTML con los valores recogidos
+  // 4º Inyectas el HTML en la tabla que ya existe
+  // 5º Limpias el formulario
 }
 
 window.addEventListener('load', () => {
@@ -66,3 +68,4 @@ window.addEventListener('load', () => {
 
   //... your code goes here
 });
+
